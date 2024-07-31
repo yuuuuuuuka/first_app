66f7261870022b3b98ac1308c54a1738ff0bf5c7
yuuuuuuuka <yuuuuka.02.29@gmail.com>
2024-07-31 20:42:30 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,15 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(content: params[:content])
+     redirect_to "/posts"
+     
+  end
+  
+end
